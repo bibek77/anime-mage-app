2877cccc9499141f3f3ce7ba3362bce738526c11
bibek77 <bibekrana01@gmail.com>
2022-02-26 23:45:32 +0530

    featured anime row added

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,12 +1,16 @@
-import 'package:anime_mage/globalConstants/constants.dart';
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+import 'package:anime_mage/globalConstants/constants.dart';
+
 import 'button_titles.dart';
 import 'header_search_box.dart';
+import 'main_characters.dart';
 
 class AppBody extends StatelessWidget {
   const AppBody({Key? key}) : super(key: key);
@@ -22,52 +26,38 @@ class AppBody extends StatelessWidget {
             title: 'Trending Characters',
             press: () {},
           ),
-          Container(
-            margin: EdgeInsets.only(
-                left: appDefaultPadding,
-                top: appDefaultPadding / 2,
-                // right: appDefaultPadding * 2,
-                bottom: appDefaultPadding * 2.5),
-            width: size.width * 0.3,
-            child: Column(
-              children: [
-                Image.asset("assests/images/eren-yeager-full-size.png"),
-                Container(
-                  padding: EdgeInsets.only(right: 5),
-                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
-                    BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: appPrimayColor.withOpacity(0.25))
-                  ]),
-                  child: Row(
-                    children: [
-                      RichText(
-                          text: TextSpan(children: [
-                        TextSpan(
-                            text: "Eren Yeager\n",
-                            style: Theme.of(context).textTheme.button),
-                        TextSpan(
-                            text: "Attack On Titan",
-                            style: TextStyle(
-                                color: appPrimayColor.withOpacity(0.5)))
-                      ])),
-                      Spacer(),
-                      Text(
-                        'SR',
-                        style: Theme.of(context)
-                            .textTheme
-                            .button
-                            ?.copyWith(color: appPrimayColor),
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
+          MainCharacters(),
+          TitleWithButton(title: "Featured Animes", press: () {}),
+          FeaturedAnimeCards(image: "assests/images/anime/attack-on-titan.jpeg",)
         ],
       ),
+    );
+  }
+}
+
+class FeaturedAnimeCards extends StatelessWidget {
+  const FeaturedAnimeCards({
+    Key? key,
+    required this.image,
+    // required this.press,
+  }) : super(key: key);
+  final String image;
+  // final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+          left: appDefaultPadding,
+          top: appDefaultPadding / 2,
+          bottom: appDefaultPadding / 2),
+      width: size.width * 0.8,
+      height: 185,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(
+              image: AssetImage(image))),
     );
   }
 }
