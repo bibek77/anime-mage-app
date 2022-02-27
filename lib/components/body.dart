cab95b51765ebc5397cbe52fe4f2bc2f1536c73f
bibek77 <bibekrana01@gmail.com>
2022-02-27 12:26:32 +0530

    details page design

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -1,14 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:velocity_x/velocity_x.dart';
-
 import 'package:anime_mage/globalConstants/constants.dart';
-
 import 'button_titles.dart';
+import 'featured_animes.dart';
 import 'header_search_box.dart';
 import 'main_characters.dart';
 
@@ -28,36 +22,13 @@ class AppBody extends StatelessWidget {
           ),
           MainCharacters(),
           TitleWithButton(title: "Featured Animes", press: () {}),
-          FeaturedAnimeCards(image: "assests/images/anime/attack-on-titan.jpeg",)
+          FeaturedAnimes(),
+          SizedBox(
+            height: appDefaultPadding,
+          )
         ],
       ),
     );
   }
 }
 
-class FeaturedAnimeCards extends StatelessWidget {
-  const FeaturedAnimeCards({
-    Key? key,
-    required this.image,
-    // required this.press,
-  }) : super(key: key);
-  final String image;
-  // final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(
-          left: appDefaultPadding,
-          top: appDefaultPadding / 2,
-          bottom: appDefaultPadding / 2),
-      width: size.width * 0.8,
-      height: 185,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-              image: AssetImage(image))),
-    );
-  }
-}
